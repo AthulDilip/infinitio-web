77162152be8b78632d89738bd89caab3efaacddc
AthulDilip <athuldilip@outlook.com>
2016-11-12 11:45:50 +0530

    Just started

---
+++ application/models/InterestModel.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ss
+ * Date: 29/7/16
+ * Time: 1:45 PM
+ */
+
+/**
+ * @property InventoryModel $InventoryModel
+ * @property ProductModel $ProductModel
+ * @property CategoryModel $CategoryModel
+ * @property FilterModel $FilterModel
+ * @property AdminModel $AdminModel
+ * @property AttributeModel $AttributeModel
+ * @property EmailModel $EmailModel
+ * @property LanguageModel $LanguageModel
+ * @property UsersModel $UsersModel
+ * @property CI_Session $session
+ * @property CI_URI $uri
+ * @property Valid $valid
+ * @property Util $util
+ * @property Urls $urls
+ * @property CI_DB_driver $db
+ * @property CI_Input $input
+ */
+
+class InterestModel extends CI_Model
+{
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->library('valid');
+    }
+
+    public function postScore($userId,$sports,$buisness,$politics,$tech,$entertainment){
+        $sql = "UPDATE user";
+    }
+}
